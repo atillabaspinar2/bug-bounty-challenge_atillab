715f43fea334191e95bc85420eb14bf33d36aa2e
atilla baspinar <atillabaspinar@gmail.com>
2026-06-08 11:24:32 +0200

    write test for bug2 (bug1 requires no test)

---
+++ src/__tests__/Bug2.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import Home from '../pages/Home';
+import { TestWrapper } from './testUtils';
+
+test('intro text renders "known" inside a <strong> element', () => {
+  const { container } = render(<Home />, { wrapper: TestWrapper });
+  const strong = container.querySelector('strong');
+  expect(strong).toBeInTheDocument();
+  expect(strong).toHaveTextContent('known');
+});
+
+test('intro text does not render raw HTML tags as literal text', () => {
+  render(<Home />, { wrapper: TestWrapper });
+  expect(screen.queryByText(/<b>known<\/b>/)).not.toBeInTheDocument();
+});
